a10c282b5e96d9013867c665bba511456f1628e8
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-14 01:50:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges (badges) {
+  for (let i = 0; i < badges.length; i++) {
+    console.log(`Welcome ${badges[i]}! You are employee #${i + 1}.`);
+  }
+
+  return badges;
+}
+
+
+
+function tailsNeverFails(){
+
+  i = 0;
+  while (Math.random() >= 0.5) {
+    i ++;
+  }
+  return `You got ${i} tails in a row!`
+}
